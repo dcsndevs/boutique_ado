6f195ffa610a79a5d774b88e1ce7960495e7b486
Dickson <olejemedickson@gmail.com>
2024-04-05 11:23:59 +0100

    Added class methods to webhook handler and webhook view

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -187,7 +187,7 @@ STANDARD_DELIVERY_PERCENTAGE =10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY' '')
 # STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY' '')
 
