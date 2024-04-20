96116d0df8a7624a119a4c7d6ef60218ad3aeee6
Dickson <olejemedickson@gmail.com>
2024-04-20 12:07:15 +0100

    add secret key and host

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -14,12 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boutique.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['boutique-ado-hvfc.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
